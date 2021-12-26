b7a129d40243311be9507385093ff12eb5eb0ec6
Ayush1285 <ayush.singh@students.vnit.ac.in>
2021-12-26 19:38:14 +0530

    modified pkg

--- src/Astarplanner.py
+++ src/Astarplanner.py
@@ -4,7 +4,12 @@ import math
 from nav_msgs.msg import OccupancyGrid
 import matplotlib.pyplot as plt
 from matplotlib import colors
+import scipy.signal as signal
 import numpy as np
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+from math import sqrt
+from rdp import runrdp
 
 
 class Astar:
@@ -13,7 +18,7 @@ class Astar:
         self.map = np.ones((120,120))*(0)
         self.resolution = resolution
         self.rr = robotradius
-        self.Rd = robotradius + 0.2
+        self.Rd = robotradius + 0.1
         self.collisioncostmap = np.zeros((self.map.shape))
         self.krep = 20
         self.parent = np.zeros((120,120))    
@@ -34,11 +39,27 @@ class Astar:
                                 self.collisioncostmap[othercell[0], othercell[1]] = max(self.collisioncostmap[othercell[0], othercell[1]],\
                                 self.krep*((self.Rd/self.resolution - distancebtwcells)/(self.Rd/self.resolution - self.rr/self.resolution))**2)
 
+   
+    def linesegment(self, p1, p2):
 
-    
+        length = sqrt((p1[1]-p2[1])**2 + (p1[0]-p2[0])**2)
+
+        if p2[0] == p1[0]:
+            py = np.linspace(p1[1],p2[1], int(length/0.05) )
+            px = np.ones(len(py))*p1[0]
+            return np.c_[px[1:],py[1:]]
+
+
+        slope = ((p2[1]-p1[1])/(p2[0]-p1[0]))
+        px = np.linspace(p1[0],p2[0],int(length/0.05))
+        py = slope*(px - np.ones(len(px))*p1[0]) + np.ones(len(px))*p1[1]
+
+        return np.c_[px[1:],py[1:]]
+
     def astarplanner(self, start, goal, map):
         self.map = map
         self.updatecollisioncostmap()
+        #self.collisioncostmap = self.collisioncostmap*0
 
 
 
@@ -66,8 +87,8 @@ class Astar:
                     self.parent[min(i+1,119),j] = int(120*i+j)
             
             if self.map[min(i+1,119),min(j+1,119)] in [-1,0]:
-                if self.gcost[min(i+1,119),min(j+1,119)] > self.gcost[i,j] + 1.414 :
-                    self.gcost[min(i+1,119),min(j+1,119)] = self.gcost[i,j] + 1.414
+                if self.gcost[min(i+1,119),min(j+1,119)] > self.gcost[i,j] + 1.4 :
+                    self.gcost[min(i+1,119),min(j+1,119)] = self.gcost[i,j] + 1.4
                     self.fcost[min(i+1,119),min(j+1,119)] = self.gcost[min(i+1,119),min(j+1,119)] + \
                         np.linalg.norm(np.array([min(i+1,119),min(j+1,119)])-goal) + self.collisioncostmap[min(i+1,119),min(j+1,119)]
 
@@ -82,8 +103,8 @@ class Astar:
                     self.parent[i,min(j+1,119)] = int(120*i+j)
 
             if self.map[max(i-1,0),min(j+1,119)] in [-1,0]:
-                if self.gcost[max(i-1,0),min(j+1,119)] > self.gcost[i,j] + 1.414 :
-                    self.gcost[max(i-1,0),min(j+1,119)] = self.gcost[i,j] + 1.414
+                if self.gcost[max(i-1,0),min(j+1,119)] > self.gcost[i,j] + 1.4 :
+                    self.gcost[max(i-1,0),min(j+1,119)] = self.gcost[i,j] + 1.4
                     self.fcost[max(i-1,0),min(j+1,119)] = self.gcost[max(i-1,0),min(j+1,119)] + \
                         np.linalg.norm(np.array([max(i-1,0),min(j+1,119)])-goal) + self.collisioncostmap[max(i-1,0),min(j+1,119)]
 
@@ -98,8 +119,8 @@ class Astar:
                     self.parent[max(i-1,0),j] = int(120*i+j)
 
             if self.map[max(i-1,0),max(j-1,0)] in [-1,0]:
-                if self.gcost[max(i-1,0),max(j-1,0)] > self.gcost[i,j] + 1.414 :
-                    self.gcost[max(i-1,0),max(j-1,0)] = self.gcost[i,j] + 1.414
+                if self.gcost[max(i-1,0),max(j-1,0)] > self.gcost[i,j] + 1.4 :
+                    self.gcost[max(i-1,0),max(j-1,0)] = self.gcost[i,j] + 1.4
                     self.fcost[max(i-1,0),max(j-1,0)] = self.gcost[max(i-1,0),max(j-1,0)] + \
                         np.linalg.norm(np.array([max(i-1,0),j])-goal) + self.collisioncostmap[max(i-1,0),max(j-1,0)]
 
@@ -114,19 +135,65 @@ class Astar:
                     self.parent[i,max(j-1,0)] = int(120*i+j)
 
             if self.map[min(i+1,119),max(j-1,0)] in [-1,0]:
-                if self.gcost[min(i+1,119),max(j-1,0)] > self.gcost[i,j] + 1.414 :
-                    self.gcost[min(i+1,119),max(j-1,0)] = self.gcost[i,j] + 1.414
+                if self.gcost[min(i+1,119),max(j-1,0)] > self.gcost[i,j] + 1.4 :
+                    self.gcost[min(i+1,119),max(j-1,0)] = self.gcost[i,j] + 1.4
                     self.fcost[min(i+1,119),max(j-1,0)] = self.gcost[min(i+1,119),max(j-1,0)] + \
                         np.linalg.norm(np.array([min(i+1,119),j])-goal) + self.collisioncostmap[min(i+1,119),max(j-1,0)]
 
                     self.parent[min(i+1,119),max(j-1,0)] = int(120*i+j)
 
-        path = [self.parent[i,j]]
+        pathros = Path()
+        pathros.header.frame_id = "map"
+        path = [int(self.parent[i,j])]
+        ypos,xpos = np.unravel_index(path[0],self.map.shape)
+        ypos = [self.resolution*ypos]
+        xpos = [self.resolution*xpos]
+        points = [[xpos[0],ypos[0]]]
         while path[0] != 120*start[0]+start[1] :
+            #pose = PoseStamped()
+            #pose.header.frame_id = 'map'
             i,j = np.unravel_index(int(path[0]), self.map.shape, order='C')
-            path = [self.parent[i,j]] + path
+            path = [int(self.parent[i,j])] + path
+            yp,xp = np.unravel_index(path[0],self.map.shape)
+            xp = xp*self.resolution
+            yp = yp*self.resolution
+            points = [[xp,yp]]+points
+            xpos = [xp]+xpos
+            ypos = [yp]+ypos
+            #pose.pose.position.x = self.resolution*xp
+            #pose.pose.position.y = self.resolution*yp
+            #pathros.poses.append(pose)
 
-        return path
+        lines = runrdp(points,0.1)
+        lines = np.array(lines)
+        
+        for i in range(len(lines[:,0])-1):
+            linesegment = self.linesegment(lines[i,:],lines[i+1,:])
+            if i == 0:
+                new_lines = np.reshape(lines[i,:],(1,2))
+                print(new_lines.shape)
+            new_lines = np.r_[new_lines,linesegment,lines[i+1,:].reshape((1,2))]
+
+
+
+
+        z = np.polyfit(new_lines[:,0], new_lines[:,1], 8)
+        f = np.poly1d(z)
+        x_new = np.linspace(new_lines[0,0], new_lines[-1,0], len(new_lines[:,0]))
+        y_new = f(x_new)
+        for i in range(len(new_lines[:,0])):
+        #for i in range(len(xpos)):
+            pose = PoseStamped()
+            pose.header.frame_id = 'map'
+            pose.pose.position.x = new_lines[i,0]
+            #pose.pose.position.x = x_new[i]
+            #pose.pose.position.x = xpos[i]
+
+            #pose.pose.position.y = ypos[i]
+            #pose.pose.position.y = y_new[i]
+            pose.pose.position.y = new_lines[i,1]
+            pathros.poses.append(pose)
+        return pathros
 
 
 
--- src/runastar.py
+++ src/runastar.py
@@ -10,7 +10,7 @@ from genmap import generatemap
 from Astarplanner import Astar
 import rospy
 from tf.transformations import quaternion_from_euler
-
+import timeit
 
 map = generatemap()
 mapdata = map.flatten()
@@ -19,40 +19,19 @@ mapdata = mapdata.astype('int8')
 
 def main():
 
-    def goalcb(data):
-        navgoal = np.array([int(data.pose.position.y/0.08),int(data.pose.position.x/0.08)])
-        path = planner.astarplanner(start, navgoal, map)
 
-        pathmessage = generatepath(path)
-        pathpub.publish(pathmessage)
 
     def startcb(data):
         global start
         start = np.array([int(data.pose.pose.position.y/0.08),int(data.pose.pose.position.x/0.08)])
+        
 
-    def generatepath(path):
-        pathmsg = Path()
+    def goalcb(data):
+        navgoal = np.array([int(data.pose.position.y/0.08),int(data.pose.position.x/0.08)])
+        path = planner.astarplanner(start, navgoal, map)
+        
+        pathpub.publish(path)
 
-        path = [int(path) for path in path]
-        ypos,xpos = np.unravel_index(path,map.shape)
-        xpos = xpos*resolution
-        ypos = ypos*resolution
-        r_x, r_y, r_yaw, r_k, travel = calc_2d_spline_interpolation(xpos, ypos, len(xpos))
-
-        for i in range(len(xpos)):
-            pose = PoseStamped()
-            pose.header.frame_id = 'map'
-            pose.pose.position.x = r_x[i]
-            pose.pose.position.y = r_y[i]
-            q = quaternion_from_euler(0, 0, r_yaw[i])
-            pose.pose.orientation.x = q[0]
-            pose.pose.orientation.y = q[1]
-            pose.pose.orientation.z = q[2]
-            pose.pose.orientation.w = q[3]
-            pathmsg.poses.append(pose)
-
-        pathmsg.header.frame_id = 'map'
-        return pathmsg
 
     rospy.init_node('astar',anonymous=True)
     rate = rospy.Rate(20.0)
@@ -62,11 +41,9 @@ def main():
     pathpub = rospy.Publisher('/path', Path, queue_size=1)
     mapmsg = OccupancyGrid()
     
-
-    robotradius = 0.2
+    robotradius = 0.3
     resolution = 0.08
     planner = Astar(resolution,robotradius)
-
 
     mapmsg.header.frame_id = 'map'
     mapmsg.info.height = 120
